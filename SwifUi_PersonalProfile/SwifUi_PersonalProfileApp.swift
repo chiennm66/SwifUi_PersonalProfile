933dc3f1beeab2186db38c81aa1cf0642d0122e2
Tony Pham <thanh01.pmt@gmail.com>
2025-03-29 10:33:01 +0700

    Initial Commit

---
+++ SwifUi_PersonalProfile/SwifUi_PersonalProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUi_PersonalProfileApp.swift
+//  SwifUi_PersonalProfile
+//
+//  Created by Tony Pham on 29/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwifUi_PersonalProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
